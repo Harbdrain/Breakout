7c774f5fb7dc23b574e34f7c5ad8598c1d94eefa
Harbdrain <mail@d-demchenko.ru>
2023-05-29 21:34:33 +0300

    Inline definition was superfluous

--- src/renderer.c
+++ src/renderer.c
@@ -2,17 +2,17 @@
 
 #include <SDL2/SDL.h>
 
-inline void renderer_buffer_clear(SDL_Renderer* renderer, Color color) {
+void renderer_buffer_clear(SDL_Renderer* renderer, Color color) {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b,
                            SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
 }
 
-inline void renderer_buffer_present(SDL_Renderer* renderer) {
+void renderer_buffer_present(SDL_Renderer* renderer) {
     SDL_RenderPresent(renderer);
 }
 
-inline void renderer_buffer_rect_draw(SDL_Renderer* renderer, int x, int y, int w,
+void renderer_buffer_rect_draw(SDL_Renderer* renderer, int x, int y, int w,
                                int h, Color color) {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b,
                            SDL_ALPHA_OPAQUE);
